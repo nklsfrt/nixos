aca92599df1d7cf1fc7a68d1b62ac7c6053d92e5
nklsfrt <nklsfrt@noreply.codeberg.org>
2022-11-07 20:01:07 +0100

    added firewall rules to open ports

--- ashes.nix
+++ ashes.nix
@@ -25,6 +25,12 @@
 				"2a01:4ff:ff00::add:1"
 				"2a01:4ff:ff00::add:2"
 		];
+		firewall = {
+			enable = true;
+			interfaces = {
+				"ens3".allowedTCPPorts = [ 80 443 ];
+			};
+		};
 	};
 	
 
