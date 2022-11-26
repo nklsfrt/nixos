ee49a3c22731ac3402d8084783c4a38028e65192
nklsfrt <nklsfrt@noreply.codeberg.org>
2022-11-26 17:37:01 +0000

    fix locale formatting in supportedLocales

--- timber.nix
+++ timber.nix
@@ -35,8 +35,8 @@
   i18n = {
     defaultLocale = "en_US.UTF-8";
     supportedLocales = [
-      "en_US.UTF-8"
-      "de_DE.UTF-8"
+      "en_US.UTF-8/UTF-8"
+      "de_DE.UTF-8/UTF-8"
     ];
     extraLocaleSettings = {
       LC_TIME = "de_DE.UTF-8";
