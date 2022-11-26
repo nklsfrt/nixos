215797583d4f750e7093c8c6fc74a21ad05497d0
nklsfrt <nklsfrt@noreply.codeberg.org>
2022-11-26 16:07:39 +0100

    cleaned up useless comments and tidied stuff up

--- timber.nix
+++ timber.nix
@@ -5,23 +5,27 @@
     [
       ./hardware-timber.nix
     ];
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.efiSysMountPoint = "/boot";
-  boot.loader.efi.canTouchEfiVariables = false;
-  
-  boot.supportedFilesystems = [ "zfs" ];
-  boot.zfs.extraPools = [ "vault" ];
+
+  boot = {
+    # Use the systemd-boot EFI boot loader.
+    loader = {
+      systemd-boot = {
+        enable = true;
+        # Don't keep more than 3 previous configurations as boot entries.
+        configurationLimit = 3;
+      };
+      efi = {
+        efiSysMountPoint = "/boot";
+        canTouchEfiVariables = false;
+      };
+    };
+    kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
+    kernelParams = [ "nohibernate" "nvidia-drm.modeset=1" ];
+    supportedFilesystems = [ "zfs" ];
+    zfs.extraPools = [ "vault" ];
+  }
+
   fileSystems."/home".neededForBoot = true;
-  networking.hostId = "68a8af9f";
-
-  boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
-  boot.kernelParams = [ "nohibernate" "nvidia-drm.modeset=1" ];
-
-  # networking.hostName = "nixos"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
   # Set your time zone.
   time.timeZone = "Europe/Vienna";
@@ -31,10 +35,6 @@
    extraOptions = lib.optionalString (config.nix.package == pkgs.nixFlakes)
      "experimental-features = nix-command flakes";
   };
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Select internationalisation properties.
   i18n = {
@@ -59,28 +59,14 @@
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
-  services.xserver.videoDriver = "nvidia";
+  services.xserver.videoDrivers =  [ "nvidia" ];
   hardware.opengl.enable = true;
   hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
 
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-  
 
-  # Configure keymap in X11
-  # services.xserver.layout = "us";
-  # services.xserver.xkbOptions = {
-  #   "eurosign:e";
-  #   "caps:escape" # map caps to escape.
-  # };
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # sound.enable = true;
-  # hardware.pulseaudio.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -89,22 +75,9 @@
   	pulse.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  # users.users.jane = {
-  #   isNormalUser = true;
-  #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-  #   packages = with pkgs; [
-  #     firefox
-  #     thunderbird
-  #   ];
-  # };
-
   users.users.nase = {
     isNormalUser = true;
-    extraGroups = ["wheel"];
+    extraGroups = [ "wheel" ];
     shell = pkgs.fish;
     hashedPassword = "$6$p6HY8TeVeozfWc4g$L0YDMa4zaU83L5rNxfesj.sGCn8bo3yB93VJxQ8/gslgScGq7BTeHsOngb3gRBCsbs4F5oRP/ywIyQZeoPUGF/";
   };
@@ -112,27 +85,15 @@
   programs.fish.enable = true;
   
   users.users.root.hashedPassword = "$6$GKt/5QJ1wA0.E/Zw$g5oPpo42B1KOm547s2wvEwpw8Us7bP4FvfPkZPKx3jKaAP57Sis/MzxgBXmvZ2WyTCInIEsF2cQG1SE3jiYMg0";
+  
   # Enable unfree packages
-
   nixpkgs.config.allowUnfree = true;
 
-  # nixpkgs.overlays = [ (self: super:
-  # 						{
-  # 							systemd = super.systemd.override {
-  #                 withCryptsetup = true;
-  # 								withHomed = true;
-  # 							};
-  # 						})
-  # ];
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
     (pkgs.systemd.overrideAttrs (oldAttrs: {
       withHomed = true;
     }))
      git
-     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
      wget
      fish
      foot
@@ -149,39 +110,8 @@
      signal-desktop
    ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
   networking.hostName = "timber";
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  system.copySystemConfiguration = true;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "22.05"; # Did you read the comment?
+  networking.hostId = "68a8af9f";
 
 }
 
