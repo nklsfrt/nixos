10190c9af8da388e256b07e6b5b01a1f4a5c52e2
nklsfrt <nklsfrt@noreply.codeberg.org>
2022-11-28 17:23:18 +0100

    initial commit

--- flake.nix
+++ flake.nix
@@ -4,25 +4,34 @@
 
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixos-22.11;
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs }: {
-  
-    nixosConfigurations."ashes" = nixpkgs.lib.nixosSystem {
+  outputs = { self, nixpkgs, home-manager }:
+
+    let
       system = "x86_64-linux";
-      modules = [
-        ./common.nix
-        ./ashes/ashes.nix
-      ];
+    in {
+
+    nixosConfigurations = {
+
+      ashes = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./common.nix
+         ./ashes/ashes.nix
+        ];
+      };
+
+      timber = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./common.nix
+          ./timber/timber.nix
+        ];
+      };
+
     };
-    
-    nixosConfigurations."timber" = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./common.nix
-        ./timber/timber.nix
-      ];
-    };
-    
   };
 }
