f8b25ce82c0cbfd9537aec6e7fac1ebf623e8160
Niklas Furtwängler <furtwaengler@posteo.de>
2022-11-28 20:10:20 +0100

    pinned my own fork of home-manager

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
 
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixos-22.11;
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "git+https://codeberg.com/nklsfrt/home-manager?ref=release-21.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
