1e77ec74defb9f237395edc5e2922da38349935e
nklsfrt <nklsfrt@noreply.codeberg.org>
2022-11-29 23:56:01 +0100

    make hotkey launch footclient, since foot.server

--- timber/home/dconf.nix
+++ timber/home/dconf.nix
@@ -66,7 +66,7 @@ with lib.hm.gvariant;
 
     "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
       binding = "<Super>Return";
-      command = "foot";
+      command = "footclient";
       name = "Launch Terminal";
     };
 
