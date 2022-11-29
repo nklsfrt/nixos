f3e30d79c28980117ac83cb5269d0eedf99dcd7b
nklsfrt <nklsfrt@noreply.codeberg.org>
2022-11-29 23:49:24 +0100

    added config for foot

--- timber/home/home.nix
+++ timber/home/home.nix
@@ -7,4 +7,17 @@
   home.username = "nase";
   home.homeDirectory = "/home/niklas";
   home.stateVersion = "22.05";
+
+  programs.foot = {
+    enable = true;
+    server.enable = true;
+    settings = {
+      main = {
+        term = "xterm-256color";
+        font = "Iosevka Term:size=12";
+      };
+      colors.alpha = 0.8;
+    };
+  };
+
 }
