7c0644aae29a1efcf414f0c1bda11bff32fc1df7
Niklas Furtwängler <furtwaengler@posteo.de>
2022-11-30 00:51:37 +0100

    moved language settings into home.nix

--- timber/timber.nix
+++ timber/timber.nix
@@ -47,27 +47,6 @@
   # Set your time zone.
   time.timeZone = "Europe/Vienna";
 
-  ## Select internationalisation properties.
-
-  i18n = {
-
-    defaultLocale = "en_US.UTF-8";
-
-    supportedLocales = [
-      "en_US.UTF-8/UTF-8"
-      "de_DE.UTF-8/UTF-8"
-    ];
-
-    extraLocaleSettings = {
-      LC_TIME = "de_DE.UTF-8";
-      LC_NUMERIC = "de_DE.UTF-8";
-      LC_MONETARY = "de_DE.UTF-8";
-      LC_PAPER = "de_DE.UTF-8";
-      LC_MEASUREMENT = "de_DE.UTF-8";
-    };
-
-  };
-
   ## Enable the X11 windowing system.
 
   services.xserver.enable = true;
