7305be8c8533f489022ff9a5cf4a853b241df392
Niklas Furtwängler <furtwaengler@posteo.de>
2022-11-30 01:11:31 +0100

    added config for fish shell

--- timber/home/home.nix
+++ timber/home/home.nix
@@ -56,4 +56,19 @@
     gnomeExtensions.gsconnect
     nil
   ];
+
+  programs.fish = {
+    enable = true;
+    functions = {
+      sshmux = {
+        description = "Launches a remote tmux session or attaches to an existing one.";
+        body = "ssh -t $argv systemd-run --scope --user tmux new -A -s ssh";
+      };
+      winreboot = {
+        description = "Reboot the auto generated Windows boot entry.";
+        body =  "systemctl reboot --boot-loader-menu=1 --boot-loader-entry=auto-windows";
+      };
+    };
+  };
+
 }
