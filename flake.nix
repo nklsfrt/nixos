65bfa03d5f1cd1450eea9000f8e9c8b9f7bab666
Niklas Furtwängler <furtwaengler@posteo.de>
2022-12-14 19:38:56 +0100

    added impermanence module

--- flake.nix
+++ flake.nix
@@ -3,12 +3,18 @@
   description = "My very first centralized infrastructure configuration!";
 
   inputs = {
+
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+
     home-manager.url = "github:nix-community/home-manager/release-22.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    impermanence.url = "github:nix-community/impermanence";
+    impermanence.inputs.nixpkgs.follows = "nixpkgs";
+
   };
 
-  outputs = { self, nixpkgs, home-manager }:{
+  outputs = { self, nixpkgs, home-manager, impermanence }:{
     nixosConfigurations = {
 
       ashes = nixpkgs.lib.nixosSystem {
@@ -16,11 +22,6 @@
         modules = [
           ./common.nix
           ./ashes/ashes.nix
-          # home-manager.nixosModules.home-manager
-          # {
-          #   home-manager.useUserPackages = true;
-          #   home-manager.users.nase = import ./ashes/home.nix;
-          # }
         ];
       };
 
@@ -33,6 +34,14 @@
           {
             home-manager.useUserPackages = true;
             home-manager.users.nase = import ./timber/home/home.nix;
+          }
+          impermanence.nixosModules.impermanence
+          {
+            environment.persistence."/persist" = {
+              directories = [
+                "/etc/NetworkManager/system-connections/"
+              ];
+            };
           }
         ];
       };
