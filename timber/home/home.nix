acb86a3c925aa05fa1091ee24c051b327139c230
Niklas Furtwängler <furtwaengler@posteo.de>
2022-12-17 03:29:45 +0100

    re-enabled commit signing

--- timber/home/home.nix
+++ timber/home/home.nix
@@ -53,7 +53,7 @@
     userName = "Niklas Furtwängler";
     signing = {
       key = "2FDF 6458 1DBA 9A81 366F ED34 895D 6A61 1B8A F8AB";
-      signByDefault = false;
+      signByDefault = true;
     };
   };
 
