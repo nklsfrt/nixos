f89629748f0b74baab96a80d35d3dc58f9ed1ffa
Niklas Furtwängler <furtwaengler@posteo.de>
2023-01-02 01:26:40 +0100

    fixed typo

--- driftwood/driftwood.nix
+++ driftwood/driftwood.nix
@@ -13,7 +13,7 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.loader.efi.efiSysMountPoint = "/boot";
   boot.kernelPackages = pkgs.linuxPackages_latest;
   networking.hostName = "driftwood"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
