da0afd12f591102b411beda20783a88ca7b438e4
Niklas Furtwängler <furtwaengler@posteo.de>
2023-01-02 08:26:50 +0100

    disabled fish_greeting

--- timber/home/home.nix
+++ timber/home/home.nix
@@ -100,6 +100,9 @@
   programs.fish = {
     enable = true;
     functions = {
+      fish_greeting = {
+        body = "";
+      };
       sshmux = {
         description = "Launches a remote tmux session or attaches to an existing one.";
         body = "ssh -t $argv systemd-run --scope --user tmux new -A -s ssh";
