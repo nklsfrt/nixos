292f91cd4f18ab052c44e1826af46d6eb8f1f54f
Niklas Furtwängler <furtwaengler@posteo.de>
2023-01-20 11:40:58 +0100

    removed uneccessary declaration

--- driftwood/driftwood.nix
+++ driftwood/driftwood.nix
@@ -91,11 +91,6 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
-  ];
-
   fonts.fonts = with pkgs; [
     iosevka-bin
   ];
