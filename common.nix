fc5bc35e4b969835101ac49060c49889778fe780
Niklas Furtwängler <furtwaengler@posteo.de>
2023-02-12 18:01:18 +0100

    updated sshkey for driftwood

--- common.nix
+++ common.nix
@@ -41,7 +41,7 @@
 		shell = pkgs.fish;
 		openssh.authorizedKeys.keys = [
 			"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILuhR19nZctqp3nUHjo8cKppnHbrKjePtNL3VzT8lFlg nase@timber"
-			"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOKOPhzYhIezVzCsj62dRtpttY1xBxA1zx0QlinZeHia nase@driftwood"
+			"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKE926T8orbrGuuHTs15n8ON4E5gyAv+enD8GWNWSRNW nase@driftwood"
 			];
     hashedPassword = "$6$tmq77efFOO.hfxev$s/0Ob1FwKdXUGNHloQd3ozesGLxBeMNXm0LtSaoecEuKlMJUbNqYH5UvxN4oW2dPvqxhT0JtNHNwS5DPQEJFd1";
 		packages = with pkgs; [
