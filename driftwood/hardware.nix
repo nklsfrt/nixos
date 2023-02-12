0a53c39e2028711014c91b971952a8ad95b5a8d3
nklsfrt <nklsfrt@noreply.codeberg.org>
2023-02-12 17:56:04 +0100

    switched to btrfs on LUKS

--- driftwood/hardware.nix
+++ driftwood/hardware.nix
@@ -3,16 +3,24 @@
 {
   imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usb_storage" "sd_mod" "sr_mod" "rtsx_pci_sdmmc" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "sd_mod" "sr_mod" "rtsx_pci_sdmmc" ];
   boot.kernelModules = [ "kvm-intel" ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/8c3849ac-5bf1-4578-bc80-dba5aa00fa6f";
-      fsType = "ext4";
+    { device = "/dev/disk/by-uuid/2bff79bb-135e-4f05-b401-68a89cec458f";
+      fsType = "btrfs";
+    };
+
+  boot.initrd.luks.devices."vault".device = "/dev/disk/by-uuid/eea4b73a-1d5f-4688-a59b-a8d161271f50";
+
+  fileSystems."/nix" =
+    { device = "/dev/disk/by-uuid/2bff79bb-135e-4f05-b401-68a89cec458f";
+      fsType = "btrfs";
+      options = [ "subvol=nix" "noatime" "compress=zstd" ];
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/C87B-1655";
+    { device = "/dev/disk/by-uuid/6EBE-0A68";
       fsType = "vfat";
     };
 
