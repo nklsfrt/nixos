f3b4afb692641d62578e842d754033b2d2075078
nklsfrt <nklsfrt@noreply.codeberg.org>
2023-02-18 03:18:20 +0100

    slimmed down gnome install

--- hosts/driftwood/config.nix
+++ hosts/driftwood/config.nix
@@ -39,6 +39,13 @@
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
+  
+  services.gnome = {
+    core-utilities.enable = false;
+    core-developer-tools.enable = false;
+    games.enable = false;
+  };
+
   hardware.opengl = {
   	enable = true;
   	extraPackages = with pkgs; [
