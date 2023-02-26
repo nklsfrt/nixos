08645a09c16e2c855731d6c59cd037867201ad43
Niklas Furtwängler <furtwaengler@posteo.de>
2023-02-26 22:17:09 +0100

    explicitly specified fira instead of all nerdfonts

--- user-profiles/nase/home.nix
+++ user-profiles/nase/home.nix
@@ -33,7 +33,7 @@
         settings = {
           main = {
             term = "xterm-256color";
-            font = "FiraCode Nerd Font Mono:size=12";
+            font = "FiraCode Mono:size=12";
           };
           colors.alpha = 0.8;
         };
