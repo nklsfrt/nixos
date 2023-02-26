f1133258cdbcb33b85ef42f9907ef5779cdd2dcc
nklsfrt <nklsfrt@noreply.codeberg.org>
2023-02-27 00:27:53 +0100

    fixed lurking whitepsace issues

--- hosts/common.nix
+++ hosts/common.nix
@@ -1,50 +1,50 @@
 { lib, pkgs, ... }:{
 
-	imports = [ ../user-profiles/nase ];
+  imports = [ ../user-profiles/nase ];
 
   system.stateVersion = "22.05";
 
-	boot = {
-		kernelPackages = lib.mkDefault pkgs.linuxPackages_latest;
-		loader = {
-			systemd-boot.enable = lib.mkDefault true;
-			efi.efiSysMountPoint = lib.mkDefault "/boot";
-		};
-	};
+  boot = {
+    kernelPackages = lib.mkDefault pkgs.linuxPackages_latest;
+    loader = {
+      systemd-boot.enable = lib.mkDefault true;
+      efi.efiSysMountPoint = lib.mkDefault "/boot";
+    };
+  };
 
-	console.keyMap = "de";
+  console.keyMap = "de";
 
-	time.timeZone = "Europe/Berlin";
+  time.timeZone = "Europe/Berlin";
 
-	system.autoUpgrade = {
-		enable = true;
-		dates = "daily";
-		flake = "git+https://codeberg.org/nklsfrt/nixos";
-	};
+  system.autoUpgrade = {
+    enable = true;
+    dates = "daily";
+    flake = "git+https://codeberg.org/nklsfrt/nixos";
+  };
 
-	nix.settings = {
-		experimental-features = [ "nix-command flakes repl-flake" ];
-		auto-optimise-store = true;
-		trusted-users = [ "nase" ];
-	};
+  nix.settings = {
+    experimental-features = [ "nix-command flakes repl-flake" ];
+    auto-optimise-store = true;
+    trusted-users = [ "nase" ];
+  };
 
-	nix.gc = {
-		automatic = true;
-		dates = "weekly";
-		options = "--delete-older-than 7d";
-	};
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 7d";
+  };
 
-	nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.allowUnfree = true;
 
-	i18n = {
-		defaultLocale = "en_US.UTF-8";
-		supportedLocales = [ "en_US.UTF-8/UTF-8" "de_DE.UTF-8/UTF-8" ];
-	};
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    supportedLocales = [ "en_US.UTF-8/UTF-8" "de_DE.UTF-8/UTF-8" ];
+  };
 
-	# Enable misc. services
+  # Enable misc. services
   services.openssh.enable = true;
   programs.fish.enable = true;
   programs.command-not-found.enable = false;
 
-	zramSwap.enable = true;
+  zramSwap.enable = true;
 }
