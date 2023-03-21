459cf247e81afc2ef616c67740db767f9ef48414
Niklas Furtwängler <furtwaengler@posteo.de>
2023-03-21 13:53:43 +0100

    restructured file

--- hosts/driftwood/home.nix
+++ hosts/driftwood/home.nix
@@ -1,7 +1,9 @@
 {...}: {
-  home-manager.users.nase.dconf.settings = {
-    "org/gnome/desktop/peripherals/touchpad" = {
-      tap-to-click = true;
+  home-manager.users.nase = {
+    dconf.settings = {
+      "org/gnome/desktop/peripherals/touchpad" = {
+        tap-to-click = true;
+      };
     };
   };
 }
