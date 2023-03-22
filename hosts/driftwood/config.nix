fffd4dae91ff552e8936e6e6f30ee291a1169f09
nklsfrt <nklsfrt@noreply.codeberg.org>
2023-03-22 10:17:42 +0100

    added oneshot service to set power-profile

--- hosts/driftwood/config.nix
+++ hosts/driftwood/config.nix
@@ -13,12 +13,22 @@
     ACTION=="add", SUBSYSTEM=="backlight", KERNEL=="intel_backlight", MODE="0666", RUN+="${pkgs.coreutils}/bin/chmod a+w /sys/class/backlight/%k/brightness"
   '';
 
-  systemd.user.services.set-brightness = {
-    description = "Sets the default screen brightness.";
-    wantedBy = ["gnome-session.target"];
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStart = "/bin/sh -c 'echo 200 > /sys/class/backlight/intel_backlight/brightness'";
+  systemd.user.services = {
+    set-brightness = {
+      description = "Sets the default screen brightness.";
+      wantedBy = ["gnome-session.target"];
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = "/bin/sh -c 'echo 200 > /sys/class/backlight/intel_backlight/brightness'";
+      };
+    };
+    set-power-profile = {
+      description = "Sets the default power-profile.";
+      wantedBy = ["gnome-session.target"];
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = "${pkgs.power-profiles-daemon}/bin/powerprofilesctl set power-saver";
+      };
     };
   };
 
