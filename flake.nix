e0ffefa11c049a6e0e34448389f728fae703dc10
Niklas Furtwängler <furtwaengler@posteo.de>
2023-04-15 16:07:52 +0200

    switched to unstable branch

--- flake.nix
+++ flake.nix
@@ -2,12 +2,11 @@
   description = "nklsfrt's personal flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
-    home-manager.url = "github:nix-community/home-manager/release-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     impermanence.url = "github:nix-community/impermanence";
     sops-nix.url = "github:Mic92/sops-nix";
-    sops-nix.inputs.nixpkgs-stable.follows = "nixpkgs";
     website = {
       url = "git+https://codeberg.org/nklsfrt/nklsfrt.de?ref=main";
       flake = false;
