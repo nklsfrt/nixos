ae7a4a09866a789c36cab4b7f78817d363820446
Niklas Furtwängler <furtwaengler@posteo.de>
2023-04-20 11:22:08 +0200

    added comfy nix shortcuts

--- users/nase/fish.nix
+++ users/nase/fish.nix
@@ -1,8 +1,16 @@
-{...}: {
+{pkgs, ...}: {
   programs.fish = {
     enable = true;
     functions = {
       fish_greeting.body = "";
+      nsh = {
+        description = "Short hand for entering a nix shell.";
+        body = "command bash -c \"${pkgs.nix}/bin/nix shell nixpkgs#$argv[1]\"";
+      };
+      nrun = {
+        description = "Short hand for running a nixpkgs application on demand.";
+        body = "command bash -c \"${pkgs.nix}/bin/nix run nixpkgs#$argv[1]\"";
+      };
       sshmux = {
         description = "Launches a remote tmux session or attaches to an existing one.";
         body = "ssh -t $argv systemd-run --scope --user tmux new -A -s ssh";
