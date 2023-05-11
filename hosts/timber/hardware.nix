ff60a015f5f33ab2976edfdf3fcad0bac7798f3f
nklsfrt <nklsfrt@noreply.codeberg.org>
2023-05-11 16:01:42 +0200

    changed governor to performance

--- hosts/timber/hardware.nix
+++ hosts/timber/hardware.nix
@@ -43,6 +43,6 @@
   networking.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+  powerManagement.cpuFreqGovernor = "performance";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
