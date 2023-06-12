9772f528a4df45210e85fbec12062e405e93cb55
Niklas Furtwängler <furtwaengler@posteo.de>
2023-06-12 05:57:17 +0200

    added vfio config

--- hosts/timber/config.nix
+++ hosts/timber/config.nix
@@ -1,8 +1,10 @@
-{profiles, ...}: {
+{config, pkgs, profiles, ...}: {
   imports = [profiles.graphical];
 
   boot = {
-    initrd.kernelModules = ["amdgpu"];
+    initrd.kernelModules = [ "kvmfr" "vfio" "vfio_pci" "vfio_iommu_type1" "amdgpu"];
+    extraModulePackages = with config.boot.kernelPackages; [kvmfr];
+    kernelParams = ["kvmfr.static_size_mb=32""vfio-pci.ids=1002:731f,1002:ab38"];
     supportedFilesystems = ["ntfs"];
   };
 
@@ -16,6 +18,25 @@
     joinNetworks = ["abfd31bd471dbd23"];
   };
 
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu.verbatimConfig = ''
+      namespaces = []
+      cgroup_device_acl = [
+        "/dev/kvmfr0",
+        "/dev/kvm",
+        "/dev/null"
+      ]
+    '';
+  };
+
+  services.udev.extraRules = ''
+    SUBSYSTEM=="kvmfr", OWNER="nase", GROUP="kvm", MODE="0600"
+  '';
+
+  environment.systemPackages = with pkgs; [
+    virt-manager
+  ];
   environment.etc."pipewire/pipewire.conf.d/set-sample-rates.conf".text = ''
     context.properties.default.clock.allowed-rates = [41000 48000]
   '';
@@ -23,6 +44,7 @@
   environment.persistence."/persist" = {
     directories = [
       "/var/lib/zerotier-one/"
+      "/var/lib/libvirt/qemu/"
     ];
   };
 }
