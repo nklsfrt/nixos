c0bb9d220ebbcfee630a3189d7a4fccbcc031b8c
Niklas Furtwängler <furtwaengler@posteo.de>
2023-07-11 17:38:04 +0200

    pinned kvmfr src to include kernel compat patch

--- hosts/timber/config.nix
+++ hosts/timber/config.nix
@@ -1,4 +1,5 @@
 {
+  inputs,
   config,
   pkgs,
   profiles,
@@ -8,7 +9,24 @@
 
   boot = {
     initrd.kernelModules = ["kvmfr" "vfio" "vfio_pci" "vfio_iommu_type1" "amdgpu"];
-    extraModulePackages = with config.boot.kernelPackages; [kvmfr];
+    extraModulePackages = with config.boot.kernelPackages; [
+      (kvmfr.overrideAttrs (_: {
+        src = let
+          lgc-master = (
+            pkgs.looking-glass-client.overrideAttrs (_: {
+              src = pkgs.fetchFromGitHub {
+                owner = "gnif";
+                repo = "LookingGlass";
+                rev = "e32b292cc1ba089db6ed28e4d5eb0fc8cc4c2235";
+                sha256 = "sha256-7MgnhOHJNvFhtB7J6+vaSiesqI8saovFHmtUiFOQZU8=";
+                fetchSubmodules = true;
+              };
+            })
+          );
+        in
+          lgc-master.src;
+      }))
+    ];
     kernelParams = ["kvmfr.static_size_mb=32" "vfio-pci.ids=1002:731f,1002:ab38" "amd_pstate=active"];
     supportedFilesystems = ["ntfs"];
   };
