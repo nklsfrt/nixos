be0ae050d1f675d00c3622811e93ed40c80e9410
Niklas Furtwängler <furtwaengler@posteo.de>
2023-07-22 08:22:20 +0200

    added more language specific configuration

--- users/nase/vscode.nix
+++ users/nase/vscode.nix
@@ -4,20 +4,23 @@
     enableUpdateCheck = false;
     package = pkgs.vscodium;
     userSettings = {
-      "editor" = {
-        "fontSize" = 14;
-        "fontFamily" = "Fira Code";
+      "[rust]" = {
+        "editor.formatOnSave" = true;
       };
+      "editor.fontSize" = 14;
+      "editor.fontFamily" = "Fira Code";
       "git.autofetch" = true;
       "nix.enableLanguageServer" = true;
       "nix.serverPath" = "nil";
+      "python.linting.pylintEnabled" = true;
     };
     extensions = with pkgs.vscode-extensions; [
+      arrterian.nix-env-selector
+      james-yu.latex-workshop
       jnoortheen.nix-ide
       matklad.rust-analyzer
       ms-azuretools.vscode-docker
-      james-yu.latex-workshop
-      arrterian.nix-env-selector
+      ms-python.python
     ];
   };
   home.packages = with pkgs; [
