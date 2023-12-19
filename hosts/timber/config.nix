65498307c684e0f2c2ecd97f0bb8d620d3b00716
Niklas Furtwängler <furtwaengler@posteo.de>
2023-12-19 01:19:35 +0100

    enabled syncthing

--- hosts/timber/config.nix
+++ hosts/timber/config.nix
@@ -31,6 +31,11 @@
     '';
   };
 
+  services.syncthing = {
+    enable = true;
+    openDefaultPorts = true;
+  };
+
   services.udev.extraRules = ''
     SUBSYSTEM=="kvmfr", OWNER="nase", GROUP="kvm", MODE="0660"
   '';
@@ -45,6 +50,11 @@
   environment.persistence."/persist" = {
     directories = [
       "/var/lib/libvirt/qemu/"
+      { 
+        directory = "/var/lib/syncthing/";
+        user = "syncthing";
+        group = "syncthing";
+      }
     ];
   };
 }
