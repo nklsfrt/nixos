a3413d3f6fbf26b0185498357027ce865b7a70d2
nklsfrt <nklsfrt@noreply.codeberg.org>
2024-07-06 22:07:23 +0200

    fixed formatting

--- hosts/ashes/config.nix
+++ hosts/ashes/config.nix
@@ -76,7 +76,9 @@
     enable = true;
     package = pkgs.mariadb;
     settings = {
-      mysqld = {bind-address = "78.47.95.229";};
+      mysqld = {
+        bind-address = "78.47.95.229";
+      };
     };
   };
 }
