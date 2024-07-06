b774fc2880235a32ffec24e47b5230e63c529e75
Niklas Furtwängler <furtwaengler@posteo.de>
2024-07-06 22:05:53 +0200

    created virtualization module

---
+++ abilities/virtualization/default.nix
@@ -0,0 +1,35 @@
+{ pkgs, ... }:
+{
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      package = pkgs.qemu_kvm;
+      runAsRoot = true;
+      swtpm.enable = true;
+      ovmf = {
+        enable = true;
+        packages = [
+          (pkgs.OVMF.override {
+            secureBoot = true;
+            tpmSupport = true;
+          }).fd
+        ];
+      };
+    };
+  };
+
+  virtualisation.containers.enable = true;
+  virtualisation = {
+    podman = {
+      enable = true;
+      dockerCompat = true;
+      defaultNetwork.settings.dns_enabled = true;
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    dive
+    podman-tui
+    podman-compose
+  ];
+}
