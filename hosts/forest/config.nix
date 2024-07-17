f013d471afcd65c8e0c75512ba4bfd94c46c1079
Niklas Furtwängler <furtwaengler@posteo.de>
2024-07-17 19:05:59 +0200

    Basic initial rewrite of forest config.

--- hosts/forest/config.nix
+++ hosts/forest/config.nix
@@ -1,26 +1,16 @@
-{ pkgs, ... }:
-let
-  router-ip = "192.168.69.1";
-  lan-bridge = "bridge1";
-  lan1 = "lan1";
-  lan2 = "lan2";
-  lan3 = "lan3";
-  wan-nic = "wan";
-in
+{ ... }:
 {
+networking.useNetworkd = true;
 
-  networking = {
-    nat = {
-      enable = true;
-      internalInterfaces = [ lan-bridge ];
-      externalInterface = wan-nic;
-    };
-    firewall.trustedInterfaces = [ lan-bridge ];
-    nftables.enable = true;
-    useNetworkd = true;
-  };
-
-  systemd.network = {
+systemd.network =
+  let
+    wan-nic = "wan";
+    lan1 = "lan1";
+    lan2 = "lan2";
+    lan3 = "lan3";
+    lan-bridge = "bridge1";
+  in
+  {
     enable = true;
     links = {
       "10-${wan-nic}" = {
@@ -59,116 +49,23 @@ in
       "30-${lan1}" = {
         matchConfig.Name = lan1;
         networkConfig.Bridge = lan-bridge;
-        linkConfig.RequiredForOnline = "no-carrier";
+        linkConfig.RequiredForOnline = "enslaved";
       };
       "30-${lan2}" = {
         matchConfig.Name = lan2;
         networkConfig.Bridge = lan-bridge;
-        linkConfig.RequiredForOnline = "no-carrier";
+        linkConfig.RequiredForOnline = "enslaved";
       };
       "30-${lan3}" = {
         matchConfig.Name = lan3;
         networkConfig.Bridge = lan-bridge;
-        linkConfig.RequiredForOnline = "no-carrier";
+        linkConfig.RequiredForOnline = "enslaved";
       };
       "40-${lan-bridge}" = {
         matchConfig.Name = "${lan-bridge}";
-        networkConfig = {
-          IPForward = true;
-          Address = "${router-ip}/24";
-        };
-        linkConfig.RequiredForOnline = "no-carrier";
+        networkConfig.IPForward = true;
+        linkConfig.RequiredForOnline = "routable";
       };
     };
-  };
-
-  services.adguardhome = {
-    enable = true;
-    mutableSettings = false;
-    settings = {
-      # http.address = "127.0.0.1";
-      dns = {
-        bind_hosts = [ "${router-ip}" ];
-        port = 53;
-        upstream_dns = [ "quic://p2.freedns.controld.com" ];
-        bootstrap_dns = [
-          "9.9.9.10"
-          "149.112.112.10"
-        ];
-        enable_dnssec = true;
-      };
-      filtering.rewrites = [
-        {
-          domain = "*.lan";
-          answer = "${router-ip}";
-        }
-      ];
-    };
-  };
-
-  services.kea.dhcp4 = {
-    enable = true;
-    settings = {
-      option-data = [
-        {
-          name = "domain-name-servers";
-          data = "${router-ip}";
-          always-send = true;
-        }
-        {
-          name = "routers";
-          data = "${router-ip}";
-          always-send = true;
-        }
-      ];
-      interfaces-config.interfaces = [ "${lan-bridge}" ];
-      subnet4 = [
-        {
-          id = 1;
-          subnet = "192.168.69.0/24";
-          pools = [ { pool = "192.168.69.101 - 192.168.69.254"; } ];
-          reservations = [
-            {
-              hostname = "timber";
-              hw-address = "9c:6b:00:06:29:ef";
-              ip-address = "192.168.69.7";
-            }
-            {
-              hostname = "driftwood";
-              hw-address = "d0:53:49:f3:f7:12";
-              ip-address = "192.168.69.5";
-            }
-            {
-              hostname = "ZyXEL NWA50AX";
-              hw-address = "b8:ec:a3:e1:f9:e6";
-              ip-address = "192.168.69.100";
-            }
-          ];
-        }
-      ];
-    };
-  };
-
-  services.netdata = {
-    enable = true;
-    config = {
-      web = {
-        "bind to" = "127.0.0.1:19999";
-      };
-    };
-  };
-
-  services.caddy = {
-    enable = true;
-    configFile = pkgs.writeText "Caddyfile" ''
-      mon.lan {
-        tls internal
-        reverse_proxy 127.0.0.1:19999
-      }
-      agh.lan {
-        tls internal
-        reverse_proxy 127.0.0.1:3000
-      }
-    '';
   };
 }
