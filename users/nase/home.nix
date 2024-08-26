493033088387a8b954fd8c27eadedef31408a422
Niklas Furtwängler <furtwaengler@posteo.de>
2024-08-26 21:18:40 +0200

    added specification of mimeapps
    Signed-off-by: Niklas Furtwängler <furtwaengler@posteo.de>

--- users/nase/home.nix
+++ users/nase/home.nix
@@ -53,6 +53,16 @@
     };
   };
 
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "x-scheme-handler/http" = ["firefox.desktop"];
+      "x-scheme-handler/https" = ["firefox.desktop"];
+      "text/html" = ["firefox.desktop"];
+      "application/pdf" = ["zathura.desktop" "evince.desktop" "firefox.desktop"];
+    };
+  };
+
   programs.nix-index.enable = true;
 
   programs.gpg.enable = true;
