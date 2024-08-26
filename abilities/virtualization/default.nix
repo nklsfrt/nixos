ecb4d119cdebbf9db1ade19b6ba79db523973649
Niklas Furtwängler <furtwaengler@posteo.de>
2024-08-26 21:28:13 +0200

    added signed vars to ovmf

--- abilities/virtualization/default.nix
+++ abilities/virtualization/default.nix
@@ -13,6 +13,7 @@
         packages = [
           (pkgs.OVMF.override {
             secureBoot = true;
+            msVarsTemplate = true;
             tpmSupport = true;
           }).fd
         ];
