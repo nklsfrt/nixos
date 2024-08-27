bbd2531ceeabb90684ad341eff42717cc3454be0
Niklas Furtwängler <furtwaengler@posteo.de>
2024-08-27 21:29:27 +0200

    fix formatting

--- users/nase/home.nix
+++ users/nase/home.nix
@@ -55,10 +55,14 @@
   xdg.mimeApps = {
     enable = true;
     defaultApplications = {
-      "x-scheme-handler/http" = ["firefox.desktop"];
-      "x-scheme-handler/https" = ["firefox.desktop"];
-      "text/html" = ["firefox.desktop"];
-      "application/pdf" = ["zathura.desktop" "evince.desktop" "firefox.desktop"];
+      "x-scheme-handler/http" = [ "firefox.desktop" ];
+      "x-scheme-handler/https" = [ "firefox.desktop" ];
+      "text/html" = [ "firefox.desktop" ];
+      "application/pdf" = [
+        "zathura.desktop"
+        "evince.desktop"
+        "firefox.desktop"
+      ];
     };
   };
 
