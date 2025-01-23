b42ea09ffab9b324bff5657307f3e7a32f169342
nklsfrt <nklsfrt@noreply.codeberg.org>
2025-01-23 20:42:40 +0100

    removed mime declarations due to linking errors

--- users/nase/home.nix
+++ users/nase/home.nix
@@ -54,20 +54,6 @@
     };
   };
 
-  xdg.mimeApps = {
-    enable = true;
-    defaultApplications = {
-      "x-scheme-handler/http" = [ "firefox.desktop" ];
-      "x-scheme-handler/https" = [ "firefox.desktop" ];
-      "text/html" = [ "firefox.desktop" ];
-      "application/pdf" = [
-        "zathura.desktop"
-        "evince.desktop"
-        "firefox.desktop"
-      ];
-    };
-  };
-
   programs.nix-index.enable = true;
 
   programs.gpg.enable = true;
