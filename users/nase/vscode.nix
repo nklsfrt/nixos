d2e25ff0a4dd31cafe7e0e4b920b2081438ba881
Niklas Furtwängler <furtwaengler@posteo.de>
2025-02-26 18:34:17 +0100

    updated renamed options

--- users/nase/vscode.nix
+++ users/nase/vscode.nix
@@ -2,32 +2,34 @@
 {
   programs.vscode = {
     enable = true;
-    enableUpdateCheck = false;
     package = pkgs.vscodium;
-    userSettings = {
-      "[python]" = {
-        "editor.defaultFormatter" = "ms-python.black-formatter";
-        "editor.formatOnSave" = true;
+    profiles.default = {
+      enableUpdateCheck = false;
+      userSettings = {
+        "[python]" = {
+          "editor.defaultFormatter" = "ms-python.black-formatter";
+          "editor.formatOnSave" = true;
+        };
+        "[rust]" = {
+          "editor.formatOnSave" = true;
+        };
+        "editor.fontSize" = 14;
+        "editor.fontFamily" = "FiraCode Nerd Font";
+        "editor.fontLigatures" = "true";
+        "flake8.args" = [ "--max-line-length=180" ];
+        "git.autofetch" = true;
+        "nix.enableLanguageServer" = true;
+        "nix.serverPath" = "nil";
       };
-      "[rust]" = {
-        "editor.formatOnSave" = true;
-      };
-      "editor.fontSize" = 14;
-      "editor.fontFamily" = "FiraCode Nerd Font";
-      "editor.fontLigatures" = "true";
-      "flake8.args" = [ "--max-line-length=180" ];
-      "git.autofetch" = true;
-      "nix.enableLanguageServer" = true;
-      "nix.serverPath" = "nil";
+      extensions = with pkgs.vscode-extensions; [
+        jnoortheen.nix-ide
+        rust-lang.rust-analyzer
+        mkhl.direnv
+        ms-python.python
+        ms-python.black-formatter
+        myriad-dreamin.tinymist
+      ];
     };
-    extensions = with pkgs.vscode-extensions; [
-      jnoortheen.nix-ide
-      rust-lang.rust-analyzer
-      mkhl.direnv
-      ms-python.python
-      ms-python.black-formatter
-      myriad-dreamin.tinymist
-    ];
   };
   programs.direnv = {
     enable = true;
