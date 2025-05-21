9608665b0e355db04affb6040ecadda75d3f51b2
nklsfrt <nklsfrt@noreply.codeberg.org>
2025-05-21 23:46:34 +0200

    switch user for commits

--- users/nase/git.nix
+++ users/nase/git.nix
@@ -2,11 +2,12 @@
 {
   programs.git = {
     enable = true;
-    userEmail = "furtwaengler@posteo.de";
-    userName = "Niklas Furtwängler";
+    userEmail = "nklsfrt@noreply.codeberg.org";
+    userName = "nklsfrt";
     signing = {
-      key = "2FDF 6458 1DBA 9A81 366F ED34 895D 6A61 1B8A F8AB";
+      key = "598E 323C 9994 2CD5 0E81 52F7 F392 5182 5187 BFED";
       signByDefault = true;
+
     };
     ignores = [
       ".envrc"
