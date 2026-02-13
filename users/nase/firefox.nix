58e804beef1c644e3e9bdb47624b26eb234ca89a
nklsfrt <nklsfrt@noreply.codeberg.org>
2026-02-13 02:27:19 +0100

    switch to librewolf

--- users/nase/firefox.nix
+++ users/nase/firefox.nix
@@ -1,58 +1,58 @@
 { pkgs, ... }:
 let
-  profilePath = "plktvsa.nase";
+  profilePath = "x9wrayxc.nase";
 in
 {
-  programs.firefox = with pkgs; {
+  programs.firefox = {
     enable = true;
-    package = wrapFirefox firefox-unwrapped {
-      extraPolicies = {
-        DisableFirefoxStudies = true;
-        DisableFirefoxAccounts = true;
-        DisablePocket = true;
-        DisableTelemetry = true;
-        EnableTrackingProtection = {
-          Value = true;
-          Cryptomining = true;
-          Fingerprinting = true;
-        };
-        EncryptedMediaExtensions.Enabled = true;
-        FirefoxHome = {
-          Pocket = false;
-          SponsoredTopSites = false;
-          SponsoredPocket = false;
-          Highlights = false;
-          Snippets = false;
-        };
-        NoDefaultBookmarks = true;
-        OverrideFirstRunPage = "";
-        PasswordManagerEnabled = false;
-        PictureInPicture.Enabled = false;
-        UserMessaging = {
-          ExtensionRecommendations = false;
-          MoreFromMozilla = false;
-          SkipOnboarding = true;
-          UrlbarInterventions = false;
-          WhatsNew = false;
-        };
+    package = pkgs.librewolf;
+    policies = {
+      DisableFirefoxStudies = true;
+      DisableFirefoxAccounts = true;
+      DisablePocket = true;  # doesn't exist anymore
+      DisableTelemetry = true;
+      EnableTrackingProtection = {
+        Value = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+        EmailTracking = true;
+      };
+      EncryptedMediaExtensions.Enabled = true;
+      FirefoxHome = {
+        Pocket = false;
+        SponsoredTopSites = false;
+        SponsoredPocket = false;
+        Highlights = false;
+        Snippets = false;
+      };
+      NoDefaultBookmarks = true;
+      OverrideFirstRunPage = "";
+      PasswordManagerEnabled = false;
+      PictureInPicture.Enabled = false;
+      UserMessaging = {
+        ExtensionRecommendations = false;
+        MoreFromMozilla = false;
+        SkipOnboarding = true;
+        UrlbarInterventions = false;
+        WhatsNew = false;
+      };
+      GenerativeAI = {
+        Chatbot = false;
+        LinkPreviews = false;
+        TabGroups = false;
+        Enabled = false;
+        Locked = true;
       };
     };
     profiles.nase = {
       name = "nase";
       path = profilePath;
       settings = {
-        # enable resolution of .lan suffix
         "browser.fixup.domainsuffixwhitelist.lan" = true;
-        # force dark mode
         "layout.css.prefers-color-scheme.content-override" = 0;
-        # disable smooth scrolling
         "general.smoothScroll" = false;
-        # disable privacy notice on first run
         "datareporting.policy.firstRunURL" = "";
-        # various privacy and telemetry settings
         "browser.contentblocking.category" = "strict";
-        # pretty self explainatory I guess
-        # "browser.uiCustomization.state" = "{\"placements\":{\"widget-overflow-fixed-list\":[],\"unified-extensions-area\":[],\"nav-bar\":[\"back-button\",\"forward-button\",\"stop-reload-button\",\"urlbar-container\",\"downloads-button\"],\"toolbar-menubar\":[\"menubar-items\"],\"TabsToolbar\":[\"tabbrowser-tabs\",\"new-tab-button\",\"alltabs-button\"],\"PersonalToolbar\":[\"import-button\",\"personal-bookmarks\"]},\"seen\":[\"save-to-pocket-button\",\"developer-button\"],\"dirtyAreaCache\":[\"nav-bar\",\"PersonalToolbar\",\"TabsToolbar\"],\"currentVersion\":19,\"newElementCount\":4}";
         "privacy.trackingprotection.enabled" = true;
         "privacy.trackingprotection.socialtracking.enabled" = false;
         "media.videocontrols.picture-in-picture.video-toggle.enabled" = false;
@@ -137,6 +137,6 @@ in
     };
   };
   home.persistence."/persist" = {
-    directories = [ ".mozilla/firefox/${profilePath}" ];
+    directories = [ ".config/librewolf/${profilePath}" ];
   };
 }
