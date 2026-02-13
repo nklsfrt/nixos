5f03d046d461f78ec64120bfe752878105ea5099
nklsfrt <nklsfrt@noreply.codeberg.org>
2026-02-13 02:30:31 +0100

    trying out profiles

--- users/nase/vscode.nix
+++ users/nase/vscode.nix
@@ -3,30 +3,66 @@
   programs.vscode = {
     enable = true;
     package = pkgs.vscodium;
-    profiles.default = {
-      enableUpdateCheck = false;
-      userSettings = {
-        "[python]" = {
-          "editor.defaultFormatter" = "ms-python.black-formatter";
-          "editor.formatOnSave" = true;
+    profiles = {
+      default = {
+        enableUpdateCheck = false;
+        userSettings = {
+          "git.autofetch" = true;
+          "editor.fontSize" = 15;
+          "editor.fontFamily" = "Adwaita Mono";
+          "editor.fontLigatures" = true;
+          "terminal.integrated.fontSize" = 16;
+          "terminal.integrated.fontLigatures.enabled" = true;
         };
-        "[rust]" = {
-          "editor.formatOnSave" = true;
-        };
-        "editor.fontSize" = 14;
-        "editor.fontFamily" = "FiraCode Nerd Font";
-        "editor.fontLigatures" = "true";
-        "flake8.args" = [ "--max-line-length=180" ];
-        "git.autofetch" = true;
-        "nix.enableLanguageServer" = true;
-        "nix.serverPath" = "nil";
+        extensions = with pkgs.vscode-extensions; [
+          jnoortheen.nix-ide
+          mkhl.direnv
+          myriad-dreamin.tinymist
+        ];
       };
-      extensions = with pkgs.vscode-extensions; [
-        jnoortheen.nix-ide
-        rust-lang.rust-analyzer
-        mkhl.direnv
-        myriad-dreamin.tinymist
-      ];
+      rust = {
+        userSettings = {
+          "[rust]" = {
+            "editor.formatOnSave" = true;
+          };
+        };
+        extensions = with pkgs.vscode-extensions; [
+          mkhl.direnv
+          rust-lang.rust-analyzer
+        ];
+      };
+      python = {
+        userSettings = {
+          "[python]" = {
+            "editor.defaultFormatter" = "ms-python.autopep8";
+            "editor.formatOnSave" = true;
+          };
+        };
+        extensions = with pkgs.vscode-extensions; [
+          mkhl.direnv
+          # ms-python.autopep8
+          ms-python.python
+        ];
+      };
+      typst = {
+        userSettings = {
+          "editor.fontSize" = 15;
+          "editor.fontFamily" = "Adwaita Mono";
+          "editor.fontLigatures" = true;
+          "terminal.integrated.fontSize" = 16;
+          "terminal.integrated.fontLigatures.enabled" = true;
+          "[typst]" = {
+            "editor.formatOnSave" = true;
+            "tinymist.formatterMode" = "typstyle";
+            "tinymist.lint.enabled" = true;
+            "tinymist.lint.when" = "onType";
+          };
+        };
+        extensions = with pkgs.vscode-extensions; [
+          mkhl.direnv
+          myriad-dreamin.tinymist
+        ];
+      };
     };
   };
   home.persistence."/persist".directories = [ ".config/VSCodium" ];
