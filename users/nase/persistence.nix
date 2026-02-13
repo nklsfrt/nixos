e6d6577c61878c1d9b2e8dcc272c75433d139cb8
nklsfrt <nklsfrt@noreply.codeberg.org>
2026-02-13 02:29:54 +0100

    added a bunch of persistent paths

--- users/nase/persistence.nix
+++ users/nase/persistence.nix
@@ -7,17 +7,13 @@
       ".local/share/keyrings"
       ".config/gsconnect"
       ".config/Signal"
-      ".config/VSCodium"
       ".local/share/com.jeffser.Alpaca"
-      ".local/share/com.jeffser.Alpaca/tmp"
       ".local/share/fractal"
       ".local/share/TelegramDesktop"
-      {
-        directory = ".local/share/PrismLauncher";
-        method = "symlink";
-      }
+      ".local/share/PrismLauncher"
       ".local/state/wireplumber"
       ".thunderbird"
+      "Books"
       "code"
       "Documents"
       "Music"
@@ -25,10 +21,13 @@
       "Videos"
     ];
     files = [
+      "./.face"
       "./.background-image"
+      ".config/goa-1.0/accounts.conf"
       ".config/monitors.xml"
       ".config/sops/age/keys.txt"
       ".local/share/fish/fish_history"
+      ".local/share/gnome-shell/session-active-history.json"
     ];
-      };
+  };
 }
