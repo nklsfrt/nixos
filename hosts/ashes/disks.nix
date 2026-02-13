3ec30428ca5e0ee66035660542b11fd9bdc1a8ff
nklsfrt <nklsfrt@noreply.codeberg.org>
2026-02-13 03:10:19 +0100

    switch to impermanent zfs

--- hosts/ashes/disks.nix
+++ hosts/ashes/disks.nix
@@ -1,37 +1,62 @@
 {
   disko.devices = {
-    disk = {
-      main = {
-        type = "disk";
-        device = "/dev/sda";
-        content = {
-          type = "gpt";
-          partitions = {
-            boot = {
-              size = "1M";
-              type = "EF02"; # for grub MBR
+    disk.main = {
+      type = "disk";
+      device = "/dev/sda";
+      content = {
+        type = "gpt";
+        partitions = {
+          boot = {
+            size = "1M";
+            type = "EF02"; # for grub MBR
+          };
+          ESP = {
+            size = "1G";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+              mountOptions = [ "umask=0077" ];
             };
-            ESP = {
-              size = "1G";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-                mountOptions = [ "umask=0077" ];
-              };
-            };
-            root = {
-              size = "100%";
-              content = {
-                type = "filesystem";
-                format = "ext4";
-                mountpoint = "/";
-              };
+          };
+          zfs = {
+            size = "100%";
+            content = {
+              type = "zfs";
+              pool = "zroot";
             };
           };
         };
       };
     };
+    zpool = {
+      zroot = {
+        type = "zpool";
+        rootFsOptions = {
+          mountpoint = "none";
+          compression = "on";
+          acltype = "posixacl";
+          xattr = "sa";
+        };
+        options.ashift = "12";
+        postCreateHook = "zfs list -t snapshot -H -o name | grep -E '^zroot/system/root@blank$' || zfs snapshot zroot/system/root@blank;";
+        datasets = {
+          "local/nix" = {
+            type = "zfs_fs";
+            mountpoint = "/nix";
+          };
+          "system/persist" = {
+            type = "zfs_fs";
+            mountpoint = "/persist";
+          };
+          "system/root" = {
+            type = "zfs_fs";
+            mountpoint = "/";
+          };
+        };
+      };
+    };
+
   };
 }
