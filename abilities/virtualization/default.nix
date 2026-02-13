ef0ea88e146e12050ba1dde32ab827a2945431e3
nklsfrt <nklsfrt@noreply.codeberg.org>
2026-02-13 02:16:09 +0100

    remove vfio setup

--- abilities/virtualization/default.nix
+++ abilities/virtualization/default.nix
@@ -2,18 +2,6 @@
 {
   users.users.nase.extraGroups = [ "libvirtd" ];
 
-  boot = {
-    initrd.kernelModules = [
-      "vfio"
-      "vfio_pci"
-      "vfio_iommu_type1"
-    ];
-    kernelParams = [
-      "amd_iommu=on"
-      "vfio-pci.ids=03:00.0,03:00.1"
-    ];
-  };
-
   virtualisation.libvirtd = {
     enable = true;
     qemu = {
