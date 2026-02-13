e6f24f27e5ec37cd5f9f77c9caebded032da35e5
nklsfrt <nklsfrt@noreply.codeberg.org>
2026-02-13 01:04:02 +0100

    fix media properties in nautilus dialog

--- abilities/gnome/default.nix
+++ abilities/gnome/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 {
   services = {
     displayManager.gdm.enable = true;
@@ -12,6 +12,15 @@
   environment.systemPackages = with pkgs; [
     gnome-console
   ];
+
+  environment.sessionVariables.GST_PLUGIN_SYSTEM_PATH_1_0 =
+    lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0"
+      [
+        pkgs.gst_all_1.gst-plugins-good
+        pkgs.gst_all_1.gst-plugins-bad
+        pkgs.gst_all_1.gst-plugins-ugly
+        pkgs.gst_all_1.gst-libav
+      ];
 
   systemd.user.units."org.gnome.Shell@wayland.service".text = ''
     [Service]
